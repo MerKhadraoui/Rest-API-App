83d267106f4009d588a6aae5bc74c661b9abf5e9
Khadraoui Meriem <miyakhad@gmail.com>
2021-08-26 12:55:03 +0200

    create mongoose Schema

---
+++ model/studentModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const dcitDataSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+
+    trim: true,
+
+    required: [true, "Name is must"],
+  },
+  userPass: {
+    type: String,
+
+    trim: true,
+
+    required: [true, "Name is must"],
+  },
+  age: {
+    type: Number,
+    //required: true,
+    required: [true, "Write age please"],
+  },
+  fbw: {
+    type: Number,
+    //required: true,
+    required: [true, "Write age please"],
+  },
+  toolStack: Array,
+  email: {
+    type: String,
+    //required: true,
+    required: [true, "Enter your email"],
+  },
+  employeeAddedDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("studentDATA", dcitDataSchema);
