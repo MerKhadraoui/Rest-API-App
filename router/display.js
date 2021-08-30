683131ba8bfdb907201fe22430e4293b9484f44d
Khadraoui Meriem <miyakhad@gmail.com>
2021-08-30 21:14:52 +0200

    get display

--- router/display.js
+++ router/display.js
@@ -2,8 +2,12 @@ const express = require("express");
 const { modelName } = require("../model/studentModel");
 const displayRouter = express.Router();
 const StudentDATA = require("../model/studentModel");
-
+const {
+  getAllstudents,
+  getOneStudent,
+} = require("../controllers/dciStudentContoller");
 displayRouter.get("/", (req, res) => {
   res.status(200).json({ messege: "this  display data" });
 });
+displayRouter.route("/:userName").get(getOneStudent);
 module.exports = displayRouter;
